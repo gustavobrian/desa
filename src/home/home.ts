e008afef7b022f64943f98a6ead79c0010b6d3df
gustavobrian <gustavobrian@msn.com>
2016-05-29 21:02:38 -0300

    sss
    nnnn

---
+++ src/home/home.ts
@@ -0,0 +1,79 @@
+import { Component } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { Http, Headers } from '@angular/http';
+import { Router } from '@angular/router-deprecated';
+import { AuthHttp } from 'angular2-jwt';
+import { contentHeaders } from '../common/headers';
+
+let styles = require('./home.css');
+let template = require('./home.html');
+
+
+@Component({
+  selector: 'home',
+  directives: [CORE_DIRECTIVES],
+  template: template,
+  styles: [styles]
+})
+export class Home {
+  jwt: string;
+  decodedJwt: string;
+  response: string;
+  api: string;
+
+  constructor(public router: Router, public http: Http, public authHttp: AuthHttp) {
+    this.jwt = localStorage.getItem('jwt');
+    this.decodedJwt = this.jwt && window.jwt_decode(this.jwt);
+  }
+
+  logout2() {
+    localStorage.removeItem('jwt');
+    this.router.parent.navigateByUrl('/login');
+  }
+  logout() {
+    event.preventDefault();
+
+    //localStorage.removeItem('jwt');
+    //  this.router.parent.navigateByUrl('/login');
+    this.http.post('http://localhost:8888/laravel/public/logoutjwt',''
+    , { headers: contentHeaders })
+      .subscribe(
+        response => {
+          localStorage.removeItem('jwt');
+          this.router.parent.navigateByUrl('/login');
+        },
+        error => {
+          alert(error.text());
+          console.log(error.text());
+        //  this.router.parent.navigateByUrl('/login');
+        }
+      );
+  }
+  callAnonymousApi() {
+    this._callApi('Anonymous', 'http://localhost:3001/api/random-quote');
+  }
+
+  callSecuredApi() {
+    this._callApi('Secured', 'http://localhost:3001/api/protected/random-quote');
+  }
+
+  _callApi(type, url) {
+    this.response = null;
+    if (type === 'Anonymous') {
+      // For non-protected routes, just use Http
+      this.http.get(url)
+        .subscribe(
+          response => this.response = response.text(),
+          error => this.response = error.text()
+        );
+    }
+    if (type === 'Secured') {
+      // For protected routes, use AuthHttp
+      this.authHttp.get(url)
+        .subscribe(
+          response => this.response = response.text(),
+          error => this.response = error.text()
+        );
+    }
+  }
+}
